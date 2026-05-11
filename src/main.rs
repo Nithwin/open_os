33c60b53e29c44540d6f0474eda489b7d01a26bb
Nithwin <vmnithwin@gmail.com>
2026-05-11 22:07:13 +0530

    Update Cargo configuration and add target specification for NyxOS

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-// Telling os Not to use standard library and we're on our own
-// We're doing this because we're building an os which was an bare metal has nothing
 #![no_std]
-
-// This tell not to start with main function rather we will tell the program where to start from
 #![no_main]
 
 use core::panic::PanicInfo;
 
-// This function is called when the program crashes.
-// Because we have no standard library, we have to define exactly what a crash does!
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        
-    }
+    loop {}
 }
 
-
-// This is the actual entry point of our Operating System.
-// `extern "C"` tells the compiler to use the standard C calling convention, 
-// which is what the hardware bootloader expects.
+// We use unsafe(no_mangle) to satisfy modern Rust requirements,
+// ensuring the bootloader can find this exact function name.
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    loop {
-        
+    // Pointer to the hardware VGA buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    let text = b"System Online: Welcome to NyxOS!";
+
+    for (i, &byte) in text.iter().enumerate() {
+        unsafe {
+            // Write the character
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Write the color (0x0A is Light Green)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb0; 
+        }
     }
+
+    loop {}
 }
